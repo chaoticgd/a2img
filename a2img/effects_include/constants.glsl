f23e226b9b53ca85fc5789e8a69545e254dd8e5e
choaticgd <chaoticgd@protonmail.com>
2018-10-12 21:44:40 +0100

    License header

--- a2img/effects_include/constants.glsl
+++ a2img/effects_include/constants.glsl
@@ -1,2 +1,20 @@
+/*
+	a2img - a free and open source image editor
+	Copyright (C) 2018 chaoticgd
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define C_PI 3.14159265359
 #define C_E  2.71828182846
